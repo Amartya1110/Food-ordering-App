14a76b50e3a81054a7995f7c3209485167656abb
Amartya1110 <amartyakhan999@gmail.com>
2023-11-14 12:23:38 +0530

    updated Empty Cart component

--- src/components/Cart/EmptyCart.js
+++ src/components/Cart/EmptyCart.js
@@ -1,4 +1,7 @@
+import { Link } from "react-router-dom"
 
+// Images
+import emptyCartImg from "../../../public/assets/emptyCartImg.jpg"
 
 // CSS
 import "./EmptyCart.css"
@@ -6,7 +9,15 @@ import "./EmptyCart.css"
 const EmptyCart = () => {
     return (
         <div className="empty-cart-wrapper">
-            Empty Cart
+            <div className="empty-cart-img-wrapper">
+                <img src={emptyCartImg} alt="empty-cart-image" className="empty-cart-img"/>
+            </div>
+            <h1 className="empty-cart-heading">Your Cart is empty</h1>
+            <div className="empty-cart-para">
+                <p>Looks like you haven't added any items to your cart</p>
+                <p>You can go to Home page to view more Restaurants</p>
+            </div>
+            <Link to="/" className="empty-cart-btn" >HomePage</Link>
         </div>
     )
 }
